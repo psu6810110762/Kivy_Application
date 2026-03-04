0a1f9ec80917910a7d7b4df453503fd4448de5c1
Ikwan Hawor <6810110762@psu.ac.th>
2026-03-04 16:51:37 +0700

    feat: draw mock snake and apple on grid

--- SnakePuzzle/main.py
+++ SnakePuzzle/main.py
@@ -4,7 +4,8 @@ from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Line
+from kivy.graphics import Color, Line, Rectangle
+
 
 
 # กำหนดขนาดหน้าจอเกม (กว้าง x สูง)
@@ -28,26 +29,35 @@ class GameBoard(Widget):
 
     def draw_grid(self, *args):
         self.canvas.before.clear()
+        self.canvas.clear()
 
+        cell = 40
+
+        # วาดพื้น + grid
         with self.canvas.before:
-            # พื้นหลังดำ
             Color(0, 0, 0, 1)
-            Line(rectangle=(self.x, self.y, self.width, self.height))
+            Rectangle(pos=self.pos, size=self.size)
 
-            # เส้น grid
             Color(0.2, 0.2, 0.2, 1)
 
-            cell = 40
-
-            # เส้นแนวตั้ง
             for x in range(0, int(self.width), cell):
                 Line(points=[self.x + x, self.y,
-                             self.x + x, self.top])
+                            self.x + x, self.top])
 
-            # เส้นแนวนอน
             for y in range(0, int(self.height), cell):
                 Line(points=[self.x, self.y + y,
-                             self.right, self.y + y])
+                            self.right, self.y + y])
+
+        # งู
+        with self.canvas:
+            Color(0, 1, 0, 1)  # เขียว
+            Rectangle(pos=(self.x + 5*cell, self.y + 5*cell),
+                    size=(cell, cell))
+
+            # แอปเปิ้ล
+            Color(1, 0, 0, 1)  # แดง
+            Rectangle(pos=(self.x + 8*cell, self.y + 8*cell),
+                    size=(cell, cell))
 
 
 class SnakeGame(Widget):
